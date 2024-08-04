3cff962ed39c0618e53a609f6815fa0b16119604
Andrey <kotlyar.andrey03@mail.ru>
2024-08-04 21:56:12 +0300

    Поработал с формами

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,6 +1,25 @@
+import { Component } from "react";
+
 import "./employees-add-form.css";
 
-const EmployeesAddForm = () => {
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: 0
+        }
+    }
+    //Получаем данные из инпутов чтобы потом создавать новых работников
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+   render() {
+        const {name, salary} = this.state;
+
     return (
         <div className="app-add-form">
             <h3>Добавьте нового сотрудника</h3>
@@ -8,16 +27,23 @@ const EmployeesAddForm = () => {
                 className="add-form d-flex">
                 <input type="text"
                     className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
+                    placeholder="Как его зовут?" 
+                    onChange={this.onValueChange}
+                    name="name"
+                    value={name}/>
                 <input type="number"
                     className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+                    placeholder="З/П в $?" 
+                    onChange={this.onValueChange}
+                    name="salary"
+                    value={salary}/>
 
                 <button type="submit"
                         className="btn btn-outline-light">Добавить</button>
             </form>
         </div>
     )
+   }
 }
 
 export default EmployeesAddForm;
